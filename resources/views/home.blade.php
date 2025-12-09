f6eb5de18d309c67d318cc501d5f337e6294d23f
Vladimír Pazdera <vladimirpazdera@Vladimirs-MacBook-Air.local>
2025-12-09 01:32:24 +0100

    added autofocus  to search input in home blade

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     <form action="{{ route('home.translate') }}" class="flex flex-wrap items-center gap-2">
-        <input type="search" name="search_text" id="search-text" class="flex-shrink sm:flex-grow sm:w-auto w-full min-w-[200px]" placeholder="search ..." value="{{ request()->search_text }}">
+        <input type="search" name="search_text" id="search-text" class="flex-shrink sm:flex-grow sm:w-auto w-full min-w-[200px]" placeholder="search ..." value="{{ request()->search_text }}" autofocus>
         <div class="lang-select-container flex gap-2 items-end flex-shrink flex-wrap sm:flex-grow sm:w-fit">
             <select name="source_language" id="source_word_language" class="flex-1 min-w-[200px]">
                 <option value="">-- Select --</option>
